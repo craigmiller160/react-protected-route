8bcb14c8f5e7dfb05d6ed7b1973f13d828681977
Craig Miller <craigmiller160@gmail.com>
2020-07-19 10:46:27 -0400

    Got first test working

--- test/ProtectedRoute.test.tsx
+++ test/ProtectedRoute.test.tsx
@@ -1,5 +1,5 @@
 import React, { ElementType } from 'react';
-import { mount, configure } from 'enzyme';
+import { mount, configure, ReactWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ProtectedRoute from '../src';
 import { MemoryRouter } from 'react-router';
@@ -49,9 +49,25 @@ const doMount = (props: MountProps) => {
     );
 };
 
+const testRoute = (component: ReactWrapper, props: MountProps) => {
+    const route: ReactWrapper = component.find('Route');
+    expect(route).toHaveLength(1);
+    expect(route.props()).toEqual(expect.objectContaining({
+        path: props.path,
+        exact: false,
+        render: expect.any(Function)
+    }));
+};
+
 describe('ProtectedRoute', () => {
     it('renders component when route doesn\'t match', () => {
-        throw new Error();
+        const props: MountProps = {
+            component: Comp,
+            path: '/hello'
+        };
+        const component = doMount(props);
+        expect(component.find('ProtectedRoute')).toHaveLength(1);
+        testRoute(component, props);
     });
 
     it('renders component with no rules when route does match', () => {
