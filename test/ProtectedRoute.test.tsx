52e2ccc6286d458aaf0c3bac2139667b4213ab4c
Craig Miller <craigmiller160@gmail.com>
2020-07-19 10:17:40 -0400

    Got tests setup

--- test/ProtectedRoute.test.tsx
+++ test/ProtectedRoute.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ElementType } from 'react';
 import { mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ProtectedRoute from '../src';
@@ -8,20 +8,41 @@ import { MemoryRouter } from 'react-router';
 
 configure({ adapter: new Adapter() });
 
+interface MountProps {
+    initialEntries?: Array<string>;
+    path: string;
+    component: ElementType;
+}
+
 const Comp = () => (
     <h1>Hello World</h1>
 );
 
-describe('First test', () => {
+const doMount = (props: MountProps) => {
+    const {
+        initialEntries,
+        path,
+        component
+    } = props;
+
+    const Component = component;
+
+    return mount(
+        <MemoryRouter initialEntries={ initialEntries ?? ['/'] }>
+            <ProtectedRoute
+                component={ Component }
+                path={ path }
+            />
+        </MemoryRouter>
+    );
+};
+
+describe('ProtectedRoute', () => {
     it('runs', () => {
-        const component = mount(
-            <MemoryRouter initialEntries={ ['/'] }>
-                <ProtectedRoute
-                    component={ Comp }
-                    path="/hello"
-                />
-            </MemoryRouter>
-        );
-        console.log(component.debug());
-    });
+        console.log('Hello');
+        doMount({
+            component: Comp,
+            path: '/hello'
+        });
+    })
 });
