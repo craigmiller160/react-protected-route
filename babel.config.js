94a57134238527a4a4ce7e945b527c7e09e0d1c4
Craig Miller <craigmiller160@gmail.com>
2020-09-01 16:46:59 -0400

    Working on dependencies and babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-
+    presets: [
+        '@babel/preset-react'
+    ],
+    plugins: [
+        '@babel/plugin-proposal-nullish-coalescing-operator',
+        '@babel/plugin-proposal-numeric-separator',
+        '@babel/plugin-proposal-optional-chaining'
+    ]
 };
